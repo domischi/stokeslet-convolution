c00b53d808b41d3b754d12e209b4de85a95cd255
Dominik Schildknecht <dominik.schildknecht@psi.ch>
2019-12-22 10:52:19 +0100

    Fix the rectangular annulus forcefield

--- forcefields.py
+++ forcefields.py
@@ -19,7 +19,7 @@ def rect_forcefield_to_center(x, a=1,b=1):
 @numba.jit(nopython=True)
 def rect_annulus_forcefield_to_center(x, a=1,b=1, ap=0.9, bp=0.9):
     x=np.array(x)
-    if (-a<=x[0] and x[0]<=-ap) or (ap<=x[0] and x[0]<=a) and (-b<=x[1] and x[1]<=-bp) or (bp<=x[1] and x[1]<=b):
+    if ((-a<=x[0] and x[0]<=-ap) or (ap<=x[0] and x[0]<=a)) and ((-b<=x[1] and x[1]<=-bp) or (bp<=x[1] and x[1]<=b)):
         return -x/norm(x)
     else:
         return np.zeros(2)
