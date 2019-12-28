538a9aeb316a1f20966bf977d2ac854032e50fca
Dominik Schildknecht <dominik.schildknecht@psi.ch>
2019-12-28 15:21:03 +0100

    Fix the boundary issue in the convolution formulation by handling the padding myself

--- stokeslet.py
+++ stokeslet.py
@@ -55,34 +55,36 @@ def get_f_on_grid(f, xmin=-3, xmax=3, ymin=-3,ymax=3, xres=20, yres=20):
         gf.append(tmp)
     return X, Y, np.array(gf)
 
-def compute_full_velocity_field_conv(f, xmin=-3, xmax=3, ymin=-3,ymax=3, xres=20, yres=20): 
-    X=np.linspace(xmin,xmax,xres)
-    Y=np.linspace(ymin,ymax,yres)
-    X, Y = np.meshgrid(X,Y)
-    hx=X[0,1]-X[0,0]
-    hy=Y[1,0]-Y[0,0]
-    ff_X = np.zeros_like(X)
-    ff_Y = np.zeros_like(X)
-    stokeslet_XX = np.zeros_like(X)
-    stokeslet_XY = np.zeros_like(X)
-    stokeslet_YY = np.zeros_like(X)
-    ind = np.zeros_like(X)
-    for i in range(len(X)):
-        for j in range(len(X[0])):
-            x=X[i,j]
-            y=Y[i,j]
-            ff_X[i,j], ff_Y[i,j] = f((x,y))
+def compute_full_velocity_field_conv(f, xmin=-3, xmax=3, ymin=-3,ymax=3, xres=20, yres=20):
+    X, Y, ff = get_f_on_grid(f, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, xres=xres, yres=yres)
+    hx_f=X[0,1]-X[0,0]
+    hy_f=Y[1,0]-Y[0,0]
+    ff_X = ff[:,:,0]
+    ff_Y = ff[:,:,1]
+    
+    s_X, s_Y, s = get_f_on_grid(stokeslet, xmin=2*xmin, xmax=2*xmax, ymin=2*ymin, ymax=2*ymax, xres=2*xres-1, yres=2*yres-1)
+    s = np.array(s)
+    hx_s=s_X[0,1]-s_X[0,0]
+    hy_s=s_Y[1,0]-s_Y[0,0]
+    stokeslet_XX = s[:,:,0,0]
+    stokeslet_XY = s[:,:,0,1]
+    stokeslet_YY = s[:,:,1,1]
+
+    assert(np.isclose(hx_s, hx_f))
+    assert(np.isclose(hy_s, hy_f))
+
+    ind = np.zeros_like(ff_X)
+    for i in range(len(ff_X)):
+        for j in range(len(ff_X[0])):
             if ff_X[i,j]  != 0 or ff_Y[i,j] != 0:
                 ind[i,j] = 1
-            S = stokeslet((x,y))
-            stokeslet_XX[i,j] = S[0,0] * hx * hy
-            stokeslet_XY[i,j] = S[0,1] * hx * hy
-            stokeslet_YY[i,j] = S[1,1] * hx * hy
-    Ux = convolve(stokeslet_XX, ff_X, mode='same') + convolve(stokeslet_XY, ff_Y, mode='same')
-    Uy = convolve(stokeslet_XY, ff_X, mode='same') + convolve(stokeslet_YY, ff_Y, mode='same')
+    Ux = convolve(stokeslet_XX, ff_X, mode='valid') + convolve(stokeslet_XY, ff_Y, mode='valid')
+    Uy = convolve(stokeslet_XY, ff_X, mode='valid') + convolve(stokeslet_YY, ff_Y, mode='valid')
+    Ux *= hx_f*hy_f
+    Uy *= hx_f*hy_f
     ## The velocity field does not make sense where there is a finite force field
-    for i in range(len(X)): 
-        for j in range(len(X[0])):
+    for i in range(len(ind)): 
+        for j in range(len(ind[0])):
             if ind[i,j]:
                 Ux[i,j]=np.nan
                 Uy[i,j]=np.nan
