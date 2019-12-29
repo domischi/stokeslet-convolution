2f1bbe36cb6c106dc68d42a3639066e08e96dca5
Dominik Schildknecht <dominik.schildknecht@psi.ch>
2019-12-29 15:02:00 +0100

    Add a noise term to the rectangular forcefield to simulate randomness

--- forcefields.py
+++ forcefields.py
@@ -8,6 +8,7 @@ def rect_forcefield_all_right(x, a=1,b=1):
     else:
         return np.zeros(2)
 
+
 @numba.jit
 def rect_forcefield_to_center(x, a=1,b=1):
     x=np.array(x)
@@ -15,6 +16,15 @@ def rect_forcefield_to_center(x, a=1,b=1):
         if norm(x)>1e-10:
             return -x/norm(x)
     return np.zeros(2)
+
+@numba.jit
+def rect_forcefield_to_center_w_noise(x, a=1,b=1, sigma=.01):
+    x=np.array(x)
+    f=np.zeros(2)
+    if -a<=x[0] and x[0]<=a and -b<=x[1] and x[1]<=b:
+        if norm(x)>1e-10:
+            f=-x/norm(x)
+    return f+np.random.normal(size=2) 
 
 @numba.jit(nopython=True)
 def rect_annulus_forcefield_to_center(x, a=1,b=1, ap=0.9, bp=0.9):
