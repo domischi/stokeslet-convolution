4eba6c8268622bf9896afa15cbaec5f48320969d
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2020-02-27 17:53:24 -0800

    Add a non-normalized force-field

--- forcefields.py
+++ forcefields.py
@@ -8,13 +8,20 @@ def rect_forcefield_all_right(x, a=1,b=1):
     else:
         return np.zeros(2)
 
-
 @numba.jit
 def rect_forcefield_to_center(x, a=1,b=1):
     x=np.array(x)
     if -a<=x[0] and x[0]<=a and -b<=x[1] and x[1]<=b:
         if norm(x)>1e-10:
             return -x/norm(x)
+    return np.zeros(2)
+
+@numba.jit
+def rect_forcefield_to_center_non_normalized(x, a=1,b=1):
+    x=np.array(x)
+    if -a<=x[0] and x[0]<=a and -b<=x[1] and x[1]<=b:
+        if norm(x)>1e-10:
+            return -x
     return np.zeros(2)
 
 @numba.jit
